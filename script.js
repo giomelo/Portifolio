039620984fd7629f652b020c0ae4e4c5e63e7d8c
Giovana Melo <giovanamelomr@gmail.com>
2021-12-03 15:26:20 -0300

    Starting Search API

--- script.js
+++ script.js
@@ -50,4 +50,56 @@ window.onload = () =>
                     }
                     
                 }
+
+                function error (err) { 
+                    console:log('Erro:', err);
+                    let divtela = document.getElementById('tela');
+                    divtela.innerHTML = "Resultado não encontrado :(";
+                }
+                
+                function executaPesquisa ()
+                {
+
+                    const queryString = 'q=' + encodeURIComponent('GitHub Octocat in:readme user:defunkt');
+                    let query = document.getElementById('txtPesquisa').value;
+
+                    let xhr = new XMLHttpRequest(); 
+                    xhr.onload = exibeResultado;
+                    xhr.onerror = error;
+                    xhr.open ('GET', ` https://api.github.com/search/repositories?q=${query}{&page,per_page,sort,order}`);
+                    xhr.send();
+
+                }
+
+                function exibeResultado()
+                    {
+                        let resu = JSON.parse(this.responseText);
+                        console.log(JSON.parse(this.responseText)); 
+                        let divtela = document.getElementById('tela');
+                        let texto = '';
+                        
+                        // //Montar texto html das noticias
+                        // let dados = JSON.parse(this.responseText);
+
+                        // for(i = 0; i< dados.articles.length; i++){
+                        //     let noticia = dados.articles[i];
+                        //     let data = new Date(noticia.publishedAt);
+                        //     texto = texto + `
+                        //     <div class="box-noticia">
+                        //     <img src="${noticia.urlToImage}" alt="">
+                        //     <span class="titulo">${noticia.title}</span><br>
+                        //     <span class="creditos">${data.toLocaleDateString ()} - ${noticia.source.name} - ${noticia.author}</span><br>
+                        //     <span class="text">
+                        //     ${noticia.content}
+                        //     </span><br>
+                        //     <a href="${noticia.url}">Leia mais ...</a>
+                        // </div>
+                        //     `;
+                        // };
+                        //preencher a div com o texto html
+
+                        divtela.innerHTML = texto;
+                }
+
+                document.getElementById('icon_search').onclick = executaPesquisa();
         }
